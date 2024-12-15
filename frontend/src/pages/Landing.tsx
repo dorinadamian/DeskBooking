b8c400aa3ada389d9c731164cef06fd6fccc07a0
DARiUSNVm <dariusmcsg123@gmail.com>
2024-12-15 23:46:13 +0200

    Modificare pagina login!

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import foto from "../assets/image 1.png";
 import foto1 from "../assets/image 2.png";
+import logo from "../assets/MyDeskHub 1.png";
 import { useNavigate } from "react-router-dom";
 import { login } from '../utils/api';
 
@@ -32,49 +33,54 @@ const Landing: React.FC = () => {
 
   return (
     <>
-      <div className="container">
-        <div className="container__left">
-          <h1>Hello, Welcome!</h1>
-        </div>
-        <div className="container__right">
-          <h1 className="title">Login</h1>
-          <form>
-            <div className="input-group">
-              <input
-                className="input__group__text"
-                type="text"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  setEmailError(""); // Clear email error on change
-                  setError(""); // Clear general error on change
-                }}
-              />
-              <img className="foto__user" src={foto} alt="foto" />
-            </div>
-            {emailError && <div className="error-message">{emailError}</div>}
-            <div className="input-group">
-              <input
-                className="input__group__text"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                  setError(""); // Clear general error on change
-                }}
-              />
-              <img className="foto__password" src={foto1} alt="foto" />
-            </div>
-          </form>
-          {error && <div className="error-message">{error}</div>}
-          <a href="#" className="forgot-password">
-            Forgot password?
-          </a>
-          <button onClick={handleNavigate} className="login-btn">
-            Login
-          </button>
+      <div className="outer-container">
+        <div className="container">
+          <div className="container__left">
+            <h1 style={{ fontSize: '3rem' }}>Hello,</h1>
+            <h2 style={{ fontSize: '2.5rem' }}>Welcome to</h2>
+            <img className="logo" src={logo} alt="mydeskhub logo" />
+            <span className="foto_title" style={{ fontSize: '1.5rem' }}>MyDeskHub</span>
+          </div>
+          <div className="container__right">
+            <h1 className="title">Login</h1>
+            <form>
+              <div className="input-group">
+                <input
+                  className="input__group__text"
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                    setEmailError(""); // Clear email error on change
+                    setError(""); // Clear general error on change
+                  }}
+                />
+                <img className="foto__user" src={foto} alt="foto" />
+              </div>
+              {emailError && <div className="error-message">{emailError}</div>}
+              <div className="input-group">
+                <input
+                  className="input__group__text"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    setError(""); // Clear general error on change
+                  }}
+                />
+                <img className="foto__password" src={foto1} alt="foto" />
+              </div>
+              </form>
+              {error && <div className="error-message">{error}</div>}
+              <a href="#" className="forgot-password">
+                Forgot password?
+              </a>
+              <button onClick={handleNavigate} className="login-btn">
+                Login
+              </button>
+          </div>
         </div>
       </div>
     </>
